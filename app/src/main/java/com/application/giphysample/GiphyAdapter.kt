dc2455fa8a8aff805406f1a15d8b627919d4f2a2
williamleebitbucket <william.lee.bitbucket@gmail.com>
2019-04-25 16:01:17 -0700

    Added search bar and filter functionality

--- app/src/main/java/com/application/giphysample/GiphyAdapter.kt
+++ app/src/main/java/com/application/giphysample/GiphyAdapter.kt
@@ -5,12 +5,21 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Filter
+import android.widget.Filterable
 import kotlinx.android.synthetic.main.giphy_list_item.view.*
 
-class GiphyAdapter(val items: ArrayList<String>, val context: Context) : RecyclerView.Adapter<ViewHolder>() {
+class GiphyAdapter(val items: ArrayList<String>, val context: Context) : RecyclerView.Adapter<ViewHolder>(), Filterable {
+
+
+    var searchItems: ArrayList<String> = ArrayList()
+
+    init {
+        searchItems = items;
+    }
 
     override fun getItemCount(): Int {
-        return items.size
+        return searchItems.size
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, p1: Int): ViewHolder {
@@ -18,7 +27,34 @@ class GiphyAdapter(val items: ArrayList<String>, val context: Context) : Recycle
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.tvGiphy.text = items.get(position)
+        holder.tvGiphy.text = searchItems.get(position)
+    }
+
+    override fun getFilter(): Filter {
+        return object: Filter() {
+            override fun performFiltering(charSequence: CharSequence): FilterResults {
+                val charString = charSequence.toString()
+                if(charString.isEmpty()) {
+                    searchItems = items
+                } else {
+                    val filteredList : ArrayList<String> = ArrayList<String>();
+                    for(row in searchItems) {
+                        if(row.toLowerCase().contains(charString.toLowerCase())) {
+                            filteredList.add(row)
+                        }
+                    }
+                    searchItems = filteredList
+                }
+                val filteredResults = Filter.FilterResults()
+                filteredResults.values = searchItems
+                return filteredResults
+            }
+
+            override fun publishResults(charSequence: CharSequence, filterResults: FilterResults) {
+                searchItems = filterResults.values as ArrayList<String>
+                notifyDataSetChanged()
+            }
+        }
     }
 }
 
--- app/src/main/java/com/application/giphysample/MainActivity.kt
+++ app/src/main/java/com/application/giphysample/MainActivity.kt
@@ -1,20 +1,26 @@
 package com.application.giphysample
 
+import android.app.SearchManager
 import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.DefaultItemAnimator
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.view.Menu
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
 import android.widget.ListView
+import android.widget.SearchView
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
 
     val animals: ArrayList<String> = ArrayList()
+    var searchView: SearchView? = null
+    var mAdapter: GiphyAdapter? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,7 +30,29 @@ class MainActivity : AppCompatActivity() {
 
         val rv_giphy_list = findViewById<RecyclerView>(R.id.giphy_list)
         rv_giphy_list.layoutManager = LinearLayoutManager(this)
-        rv_giphy_list.adapter = GiphyAdapter(animals, this)
+        mAdapter = GiphyAdapter(animals, this)
+        rv_giphy_list.adapter = mAdapter
+        rv_giphy_list.itemAnimator = DefaultItemAnimator()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        searchView = menu?.findItem(R.id.app_bar_search)?.actionView as SearchView
+        searchView!!.setSearchableInfo(searchManager.getSearchableInfo(componentName))
+        searchView!!.maxWidth = Integer.MAX_VALUE
+        searchView!!.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                mAdapter?.filter?.filter(query)
+                return false;
+            }
+
+            override fun onQueryTextChange(query: String?): Boolean {
+                mAdapter?.filter?.filter(query)
+                return false;
+            }
+        })
+        return super.onCreateOptionsMenu(menu)
     }
 
     fun addAnimals() {
@@ -61,5 +89,4 @@ class MainActivity : AppCompatActivity() {
         animals.add("penguin")
         animals.add("parrot")
     }
-
 }
