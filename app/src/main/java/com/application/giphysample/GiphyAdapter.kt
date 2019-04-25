7b945b932399453ef38b812ec2e695ac9c65e17f
williamleebitbucket <william.lee.bitbucket@gmail.com>
2019-04-25 13:05:28 -0700

    Added recyclerview with dummy values

---
+++ app/src/main/java/com/application/giphysample/GiphyAdapter.kt
@@ -0,0 +1,27 @@
+package com.application.giphysample
+
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kotlinx.android.synthetic.main.giphy_list_item.view.*
+
+class GiphyAdapter(val items: ArrayList<String>, val context: Context) : RecyclerView.Adapter<ViewHolder>() {
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, p1: Int): ViewHolder {
+        return ViewHolder(LayoutInflater.from(context).inflate(R.layout.giphy_list_item, parent, false))
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.tvGiphy.text = items.get(position)
+    }
+}
+
+class ViewHolder (view: View): RecyclerView.ViewHolder(view) {
+    val tvGiphy = view.tv_giphy
+}
